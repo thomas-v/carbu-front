f5c05ed1ac953f307d70f861ba4ce8bae93c5a8b
Thomas Vitiello <tvitiello@vps706209.ovh.net>
2020-01-03 22:54:34 +0100

    affichage des stations + prix carbu

--- static/js/stations.js
+++ static/js/stations.js
@@ -7,12 +7,13 @@ let geoloc = (options) => {
 
 // Initialisation de la carte
 let initMap = (latitude, longitude) => {
-    let macarte = L.map('map').setView([latitude, longitude], 14);
+    let map = L.map('map').setView([latitude, longitude], 14);
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
         minZoom: 1,
         maxZoom: 18
-    }).addTo(macarte);
+    }).addTo(map);
+    return map;
 }
 
 let geocode = (latitude, longitude) => {
@@ -25,6 +26,12 @@ let geocode = (latitude, longitude) => {
 let getCity = async (latitude, longitude) => {
     let geocode_value = await geocode(latitude, longitude);
     return geocode_value.address.postcode;
+}
+
+let getStationsByPostCode = (postCode) => {
+    return new Promise(function (resolve, reject) {
+        $.get('https://thomasdev.ovh/api/stations/' + postCode, resolve);
+    });
 }
 
 $( document ).ready(async () => {
@@ -45,9 +52,25 @@ $( document ).ready(async () => {
     }
 
     // affichage de la carte
-    await initMap(latitude, longitude);
+    let map = await initMap(latitude, longitude);
+    console.log(map);
 
     // recuperation de la ville
-    let city = await getCity(latitude, longitude);
-    console.log(city);
+    let postCode = await getCity(latitude, longitude);
+    
+    // on recupere la liste des stations services de la ville
+    let stations = await getStationsByPostCode(postCode);
+    stations = JSON.parse(stations);
+
+    console.log(stations);
+    
+    // on parcourt les stations pour recuperer les coordonnees
+    for(let i = 0; i < stations.length; i++){
+        let marker = L.marker([stations[i]['latitude'], stations[i]['longitude']]).addTo(map);
+        marker.bindPopup("<b>Gazole</b> : " + stations[i]['carburants']['Gazole'] + "<br><b>GPLc</b> : " + stations[i]['carburants']['GPLc']
+        + "<br><b>E10</b> : " + stations[i]['carburants']['E10'] + "<br><b>SP98</b> : " + stations[i]['carburants']['SP98']);
+    }
+
+    //L.marker([latitude, longitude]).addTo(map);
+
 });
